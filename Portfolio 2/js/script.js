6cbc2bdce3a26988ab53c214fa2b15a93d3f2b7a
Long le <longle10800@gmail.com>
2024-05-12 20:14:32 -0400

    Add files via upload

---
+++ Portfolio 2/js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["Full Stack Developer", "Web Developer", "Data Analyst"],
+    typeSpeed:100,
+    backSpeed: 60,
+    loop: true
+})
